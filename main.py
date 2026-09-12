8146142aea9f8c0740d1c635ca58fded54afd15c
FriedAxons <friedaxons@proton.me>
2026-09-12 09:24:21 -0500

    Add DELETE endpoint

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ if not os.path.isfile(db_file):
 
 app = FastAPI()
 
+
 @app.put("/db")
 async def put(key: str, value: str):
     db = None
@@ -25,6 +26,7 @@ async def put(key: str, value: str):
     f.close()
     return value
 
+
 @app.get("/db")
 async def get(key: str):
     db = None
@@ -32,8 +34,35 @@ async def get(key: str):
         db = pickle.load(f)
     f.close()
     if db is None:
-        raise HTTPException(status_code=404, detail=f"Database file {db_file} could not be opened and loaded")
+        raise HTTPException(
+            status_code=404,
+            detail=f"Database file {db_file} could not be opened and loaded",
+        )
     val = db.get(key, None)
     if val is None:
         raise HTTPException(status_code=404, detail=f"No value set for key {key}")
+    return val
+
+
+@app.delete("/db")
+async def delete(key: str):
+    db = None
+    with open(db_file, "rb") as f:
+        db = pickle.load(f)
+    f.close()
+    if db is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Database file {db_file} could not be opened and loaded",
+        )
+    val = db.get(key, None)
+    if val is None:
+        raise HTTPException(status_code=404, detail=f"No {key} key to delete")
+
+    del db[key]
+
+    with open(db_file, "wb+") as f:
+        pickle.dump(db, f)
+    f.close()
+
     return val
