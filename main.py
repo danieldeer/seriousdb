1e08866fbd62761db3252d4fb2f50d2c73f0e1d0
Lazy <victorespinha204@gmail.com>
2026-09-12 17:44:46 +0000

    added a simple cache class for cleaner management

--- main.py
+++ main.py
@@ -3,37 +3,55 @@ import os
 from fastapi import FastAPI
 from fastapi import HTTPException
 
-db_file = ".sdb"
+class Cache:
+    def __init__(self):
+        self.filename = None
+        self.db = None
 
-# Check if the database file exists, if not populate it
-if not os.path.isfile(db_file):
-    with open(db_file, "wb") as f:
-        pickle.dump({"default": "default"}, f)
-    f.close()
+
+def insert(key: str, value: str, cache: Cache):
+    if cache.db is None:
+        raise HTTPException(status_code=404, detail=f"Database file {cache.filename} could not be opened and loaded")
+    cache.db[key] = value
+    return value
+
+def select(key: str, cache : Cache):
+    if cache.db is None:
+        raise HTTPException(status_code=404, detail=f"Database file {cache.filename} could not be opened and loaded")
+    val = cache.db.get(key, None)
+    if val is None:
+        raise HTTPException(status_code=404, detail=f"No value set for key {key}")
+    return val
+
+def load(filename: str, cache : Cache):
+    db_file = filename
+    if not os.path.isfile(db_file):
+        with open(db_file, "wb") as f:
+            pickle.dump({"default": "default"}, f)
+    else:
+        with open(filename, "rb") as f:
+            cache.db = pickle.load(f)
+    cache.filename = filename
+
+def flush(cache : Cache):
+    if cache.db is None:
+        return
+    with open(cache.filename, "wb+") as f:
+        pickle.dump(cache.db, f)
+
+db_file = ".sdb"
+cache = Cache()
+load(db_file, cache)
 
 app = FastAPI()
 
 @app.put("/db")
 async def put(key: str, value: str):
-    db = None
-    with open(db_file, "rb") as f:
-        db = pickle.load(f)
-        db[key] = value
-    f.close()
-    with open(db_file, "wb+") as f:
-        pickle.dump(db, f)
-    f.close()
+    insert(key, value, cache)
+    flush(cache)
     return value
 
 @app.get("/db")
 async def get(key: str):
-    db = None
-    with open(db_file, "rb") as f:
-        db = pickle.load(f)
-    f.close()
-    if db is None:
-        raise HTTPException(status_code=404, detail=f"Database file {db_file} could not be opened and loaded")
-    val = db.get(key, None)
-    if val is None:
-        raise HTTPException(status_code=404, detail=f"No value set for key {key}")
+    val = select(key, cache)
     return val
