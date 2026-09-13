66299ac4d2045f0f278d49942d9e9201a9eb2f4e
xqill275 <oliverlong275@gmail.com>
2026-09-13 13:49:28 +0100

    made all cache related functionts part of the cache class

--- main.py
+++ main.py
@@ -11,80 +11,79 @@ class Cache:
         self.db = None
         self.lock = Lock()
 
-
-def insert(key: str, value: str, cache: Cache):
-    with cache.lock:
-        if cache.db is None:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Database file {cache.filename} could not be opened and loaded",
-            )
-        cache.db[key] = value
-    return value
-
-
-def select(key: str, cache: Cache):
-    with cache.lock:
-        if cache.db is None:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Database file {cache.filename} could not be opened and loaded",
-            )
-        val = cache.db.get(key, None)
-    if val is None:
-        raise HTTPException(status_code=404, detail=f"No value set for key {key}")
-    return val
+    def insert(self, key: str, value: str):
+        with self.lock:
+            if self.db is None:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Database file {self.filename} could not be opened and loaded",
+                )
+            self.db[key] = value
+        return value
 
 
-def load(filename: str, cache: Cache):
-    with cache.lock:
-        db_file = filename
-        if not os.path.isfile(db_file):
-            with open(db_file, "wb") as f:
-                json_dumps = json.dumps({"default": "default"}).encode()
-                f.write(json_dumps)
-            cache.db = {"default": "default"}
-        else:
-            with open(filename, "rb") as f:
-                binary_text = f.read()
-                json_text = binary_text.decode()
-                cache.db = json.loads(json_text)
-        cache.filename = filename
-
-
-def flush(cache: Cache):
-    with cache.lock:
-        if cache.db is None:
-            return
-        with open(cache.filename, "wb+") as f:
-            json_dumps = json.dumps(cache.db).encode()
-            f.write(json_dumps)
-
-
-def delete(key: str, cache: Cache):
-    with cache.lock:
-        if cache.db is None:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Database file {cache.filename} could not be opened and loaded",
-            )
-        if key not in cache.db:
+    def select(self, key: str):
+        with self.lock:
+            if self.db is None:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Database file {self.filename} could not be opened and loaded",
+                )
+            val = self.db.get(key, None)
+        if val is None:
             raise HTTPException(status_code=404, detail=f"No value set for key {key}")
-        deleted_value = cache.db.pop(key)
-    return deleted_value
+        return val
+
+
+    def load(self, filename: str):
+        with self.lock:
+            db_file = filename
+            if not os.path.isfile(db_file):
+                with open(db_file, "wb") as f:
+                    json_dumps = json.dumps({"default": "default"}).encode()
+                    f.write(json_dumps)
+                self.db = {"default": "default"}
+            else:
+                with open(filename, "rb") as f:
+                    binary_text = f.read()
+                    json_text = binary_text.decode()
+                    self.db = json.loads(json_text)
+            self.filename = filename
+
+
+    def flush(self):
+        with self.lock:
+            if self.db is None:
+                return
+            with open(self.filename, "wb+") as f:
+                json_dumps = json.dumps(self.db).encode()
+                f.write(json_dumps)
+
+
+    def delete(self, key: str):
+        with self.lock:
+            if self.db is None:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Database file {self.filename} could not be opened and loaded",
+                )
+            if key not in self.db:
+                raise HTTPException(status_code=404, detail=f"No value set for key {key}")
+            deleted_value = self.db.pop(key)
+        return deleted_value
 
 
 db_file = ".sdb"
 cache = Cache()
-load(db_file, cache)
+cache.load(db_file)
 
 app = FastAPI()
 
 
 @app.delete("/db")
-async def delete_endpoint(key: str):
-    deleted_value = delete(key, cache)
-    flush(cache)
+async def delete(key: str):
+    deleted_value = cache.delete(key)
+    cache.flush()
     return {
         "key": key,
         "deleted_value": deleted_value,
@@ -93,12 +92,12 @@ async def delete_endpoint(key: str):
 
 @app.put("/db")
 async def put(key: str, value: str):
-    insert(key, value, cache)
-    flush(cache)
+    cache.insert(key, value)
+    cache.flush()
     return value
 
 
 @app.get("/db")
 async def get(key: str):
-    val = select(key, cache)
+    val = cache.select(key)
     return val
