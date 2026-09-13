f5427e99feff4c336e7f0b811dd1eedecfd273f8
Max00009 <mp7636681@gmail.com>
2026-09-14 02:19:24 +0530

    add config values

--- src/seriousdb/config.py
+++ src/seriousdb/config.py
@@ -1,1 +1,4 @@
-DB_FILE = ".sdb"
+DB_FILE=".sdb" #the permanent database file on disk
+WAL_FILE="changes.log" #temporary file to keep track of changes untill next compaction is done
+COMPACT_THRESHOLD=50 #after 50 new entry we update our DB_FILE on disk
+WAL_SYNC_ON_WRITE=True #make each PUT call crash safe by forcing the OS to write to WAL file.safe but slow.
