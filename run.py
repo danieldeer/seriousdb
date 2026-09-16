d30cc159831af7a7464c47bf4cc972203fb6de46
Hugo G <hg_gall@protonmail.com>
2026-09-16 11:15:45 +0200

    add port parsing

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 import uvicorn
+import argparse
 
 if __name__ == "__main__":
-    uvicorn.run("seriousdb.main:app", host="0.0.0.0", port=8000, reload=True)
+    parser = argparse.ArgumentParser(
+        prog="SeriousDB", description="Simple HTTP database"
+    )
+    parser.add_argument(
+        "-p", "--port", default=8000, type=int, help="HTTP port to bind SeriousDB"
+    )
+    args = parser.parse_args()
+    uvicorn.run("seriousdb.main:app", host="0.0.0.0", port=args.port, reload=True)
